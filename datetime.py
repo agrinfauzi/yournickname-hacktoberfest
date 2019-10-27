edc9f2df096900bea6c69c4d1fc88f80eaf035ef
Kharisma Gilang P <34505683+kharismagp@users.noreply.github.com>
2019-10-27 13:36:21 +0700

    date and time

---
+++ datetime.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+now = datetime.now()
+
+mm = str(now.month)
+
+dd = str(now.day)
+
+yyyy = str(now.year)
+
+hour = str(now.hour)
+
+mi = str(now.minute)
+
+ss = str(now.second)
