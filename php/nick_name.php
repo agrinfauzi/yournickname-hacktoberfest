4c68164dda338a96f05f99923fb75c8fd87531e9
jaelani <56112676+Jaelani4@users.noreply.github.com>
2020-10-08 13:03:24 +0700

    Update nick_name.php
    tipe data bolean

--- php/nick_name.php
+++ php/nick_name.php
@@ -1,2 +1,10 @@
 <?php
 exit('My nickname is hello world :D');
+
+
+$apakahSiswaLulus = true;
+$apakahSiswaSudahUjian = false;
+
+var_dump($apakahSiswaLulus);
+echo "<br>";
+var_dump($apakahSiswaSudahUjian);
